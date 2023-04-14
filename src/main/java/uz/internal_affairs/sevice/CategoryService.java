79f44214c748159d69419cd842641120a663a006
user <bekzodsadriddinov92@gmail.com>
2023-04-14 18:23:03 +0500

    Category123333

--- src/main/java/uz/internal_affairs/sevice/CategoryService.java
+++ src/main/java/uz/internal_affairs/sevice/CategoryService.java
@@ -3,25 +3,40 @@ package uz.internal_affairs.sevice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.internal_affairs.dto.CategoryDto;
-import uz.internal_affairs.entity.CategoryEntity;
+import uz.internal_affairs.entity.Category;
+import uz.internal_affairs.repository.CategoryRepository;
+
+import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
-public class CategoryService implements BaseService<CategoryEntity, CategoryDto>{
+public class CategoryService implements BaseService<CategoryDto,Category>{
+
+    private final CategoryRepository categoryRepository;
 
     @Override
-    public CategoryDto add(CategoryEntity categoryEntity) {
+    public Category add(CategoryDto categoryDto) {
+        Optional<Category> byName = categoryRepository.findByName(categoryDto.getName());
+        if (byName.isPresent()){
+            throw new IllegalArgumentException();
+        }
+        Category category=Category.of(categoryDto);
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public Category update(Long id, CategoryDto categoryDto) {
+        Optional<Category> byId = categoryRepository.findById(id);
 
         return null;
     }
+
 
     @Override
     public boolean delete(Long id) {
         return false;
     }
 
-    @Override
-    public CategoryDto update(Long id, CategoryEntity categoryEntity) {
-        return null;
-    }
+
 }
