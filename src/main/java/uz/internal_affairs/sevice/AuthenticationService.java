64dd03ed2aaefdf20f6210b76315c4b5ac799095
Ravshanbek <ravshanbek9918@gmail.com>
2023-04-20 10:16:57 +0500

    user update and user new save method

--- src/main/java/uz/internal_affairs/sevice/AuthenticationService.java
+++ src/main/java/uz/internal_affairs/sevice/AuthenticationService.java
@@ -1,6 +1,7 @@
 package uz.internal_affairs.sevice;
 
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import uz.internal_affairs.common.util.SecurityUtils;
 import uz.internal_affairs.config.token.JwtService;
 import uz.internal_affairs.dto.TokenResponseDto;
 import uz.internal_affairs.dto.LoginRequestDto;
@@ -14,25 +15,20 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
 
     public TokenResponseDto register(UserDto request) {
-        UserEntity user = UserEntity.builder()
-                .firstname(request.getFirstname())
-                .lastname(request.getLastname())
-                .username(request.getUsername())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .role(Role.USER)
-                .build();
-        repository.save(user);
 
-        String jwtToken = jwtService.generateToken(user);
+        String jwtToken = jwtService.generateToken(saveUser(request));
 
         return TokenResponseDto.builder()
                 .token(jwtToken)
@@ -51,6 +47,22 @@ public class AuthenticationService {
         return TokenResponseDto.builder()
                 .token(jwtToken)
                 .build();
+    }
+
+    public UserEntity saveUser(UserDto userDto){
+
+
+        Optional<UserEntity> byUsername = userRepository.findByUsername(userDto.getUsername());
+        if (byUsername.isPresent()) {
+            throw new IllegalArgumentException();
+        }
+
+        UserEntity user = userDto.toEntity("password","role");
+        user.forCreate(userRepository.findByUsername(SecurityUtils.getUsername()).orElseThrow(()-> new UsernameNotFoundException(" user name not found!")).getId());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        user.setRole(userDto.getRole() == null ? Role.USER : userDto.getRole());
+
+        return userRepository.save(user);
     }
 
 }
--- src/main/java/uz/internal_affairs/sevice/UserService.java
+++ src/main/java/uz/internal_affairs/sevice/UserService.java
@@ -7,35 +7,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import uz.internal_affairs.common.util.SecurityUtils;
 import uz.internal_affairs.dto.UserDto;
 import uz.internal_affairs.dto.UserScoreDto;
 import uz.internal_affairs.entity.UserEntity;
 import uz.internal_affairs.repository.UserRepository;
 
-import java.util.Optional;
 import java.util.*;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements BaseService<UserDto, UserEntity> {
+public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
     private Logger log = LoggerFactory.getLogger(getClass().getName());
-
-    @Override
-    public UserEntity add(UserDto userDto) {
-        Optional<UserEntity> byUsername = userRepository.findByUsername(userDto.getUsername());
-        if (byUsername.isPresent()) {
-            throw new IllegalArgumentException();
-        }
-        UserEntity userEntity = UserEntity.of(userDto);
-        userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
-        //barmoq izi
-        // rasim
-        return userRepository.save(userEntity);
-    }
 
     public List<UserDto> getUserAll() {
         List<UserDto> userResponseDto = new ArrayList<>();
@@ -55,16 +42,6 @@ public class UserService implements BaseService<UserDto, UserEntity> {
     }
 
 
-    @Override
-    public boolean delete(Long id) {
-        return false;
-    }
-
-    @Override
-    public UserEntity update(Long id, UserDto userDto) {
-        return null;
-    }
-
     public UserDto getUserInformation(Long id) {
 
         UserEntity userInformation = userRepository.getUserInformation(id);
@@ -79,26 +56,34 @@ public class UserService implements BaseService<UserDto, UserEntity> {
         return responseInformationUser;
     }
 
+    @Transactional(readOnly = true)
     public Boolean updateUser(UserDto userDto) {
         UserEntity user = userRepository.findByUsername(
                 SecurityUtils.getUsername()).orElseThrow(() ->
                 new UsernameNotFoundException("curren user username not found!")
         );
 
-        if(!Objects.equals(userDto.getId(), user.getId())) return false;
+        if (!Objects.equals(userDto.getId(), user.getId())) return false;
 
         log.atInfo().log("!Обновление... пользователя");
 
         user.forUpdate();
 
-        if(!StringUtils.isEmpty(userDto.getFirstname())) user.setFirstname(userDto.getFirstname());
-        if(!StringUtils.isEmpty(userDto.getLastname())) user.setLastname(userDto.getLastname());
-        if(!StringUtils.isEmpty(userDto.getUsername()))user.setUsername(userDto.getUsername());
-        if(!StringUtils.isEmpty(userDto.getPhoneNumber())) user.setPhoneNumber(user.getPhoneNumber());
-        if(!StringUtils.isEmpty(userDto.getMiddleName())) user.setMiddleName(user.getMiddleName());
-        if(!StringUtils.isEmpty(userDto.getPassword())) user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        if (!StringUtils.isEmpty(userDto.getFirstname())) user.setFirstname(userDto.getFirstname());
+        if (!StringUtils.isEmpty(userDto.getLastname())) user.setLastname(userDto.getLastname());
+        if (!StringUtils.isEmpty(userDto.getUsername())) user.setUsername(userDto.getUsername());
+        if (!StringUtils.isEmpty(userDto.getPhoneNumber())) user.setPhoneNumber(user.getPhoneNumber());
+        if (!StringUtils.isEmpty(userDto.getMiddleName())) user.setMiddleName(user.getMiddleName());
+        if (!StringUtils.isEmpty(userDto.getPassword()))
+            user.setPassword(passwordEncoder.encode(userDto.getPassword()));
 
         userRepository.save(user);
         return true;
+    }
+
+    @Transactional
+    public Boolean userDelete(Long id) {
+        Integer userDeleteIsSuccess = userRepository.userDelete(id);
+        return userDeleteIsSuccess > 0;
     }
 }
