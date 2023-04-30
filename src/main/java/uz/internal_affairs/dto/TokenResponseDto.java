8342af3a4a2bd51eb91f609cc73c378ba2d9f838
oxun_123 <r3331426!@#>
2023-04-30 10:33:12 +0600

    CLEAN CODING!!!

--- src/main/java/uz/internal_affairs/dto/TokenResponseDto.java
+++ src/main/java/uz/internal_affairs/dto/TokenResponseDto.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import uz.internal_affairs.entity.role.Role;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -12,4 +15,6 @@ import lombok.NoArgsConstructor;
 public class TokenResponseDto {
 
   private String token;
+
+  private List<Role> allowedRoles;
 }
